19f882a1718a8be1d8bc45848c7c2003d19f94ca
Collin <collinhallett@gmail.com>
2021-08-06 14:33:10 -0500

    added document variables and a couple functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lon=-0.127758&lat=51.507351&radius=100", {
+const searchValue = document.querySelector("#search");
+const searchBtn = document.querySelector("#searchBtn");
+const mainBody = document.querySelector("#main");
+
+getTrails = function() {
+fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lon=-0.127758&lat=51.507351&radius=25", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "53bb73ef70msh2c586d23ef2e24cp1e49c1jsn9741f86cc83c",
@@ -10,8 +15,22 @@ fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lon=-0.127758&la
         response.json().then(function(data) {
             console.log(data);
         })
+    } else {
+        alert("Error: Could not find results");
     }
 })
 .catch(err => {
 	console.error(err);
+    
+})
+};
+
+renderTrails = function(data) {
+
+}
+getTrails()
+
+searchBtn.addEventListener('click', function(){
+    console.log('hello');
 });
+
